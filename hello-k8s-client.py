d0f9dd2b2d8768e0d270b56c9d705685d821d160
davidpeprah <peprahdavid@gmail.com>
2025-05-09 01:43:50 -0400

    Added debug message

--- hello-k8s-client.py
+++ hello-k8s-client.py
@@ -5,14 +5,19 @@ from time import sleep
 SERVER = os.getenv('SERVER')
 SERVER_PORT = os.getenv('SERVER_PORT')
 
-while True:
-    sleep(2)
-    try:
-      response = requests.get(f'http://{SERVER}:{SERVER_PORT}')
+if SERVER_PORT and SERVER:
+    print(f"{SERVER}:{SERVER_PORT}")
+    while True:
+        sleep(2)
+        try:
+            response = requests.get(f'http://{SERVER}:{SERVER_PORT}')
 
-      if response.status_code == 200:
-          print(response.text)
-      else:
-          print(f"Failed with status code: {response.status_code}") 
-    except Exception as e:
-        print('Service not available')
+            if response.status_code == 200:
+                print(response.text)
+            else:
+                print(f"Failed with status code: {response.status_code}") 
+        except Exception as e:
+            print('Service not available')
+        
+else:
+    print('Environmental Variables not available')
